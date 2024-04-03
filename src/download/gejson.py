b7bd2db860a01c7a04cb6bd3856db50a278a84ec
tjhon <fr.jhonk@gmail.com>
2024-04-03 13:16:12 -0500

    population

--- src/download/gejson.py
+++ src/download/gejson.py
@@ -1,5 +1,11 @@
 import requests
 
+raster_pop_url = "https://data.worldpop.org/GIS/Population/Global_2000_2020_Constrained/2020/maxar_v1/ETH/eth_ppp_2020_constrained.tif"
+raster_pop = requests.get(raster_pop_url).content
+
+with open("./Data/Raster/Population/eth_ppp_2020_constrained.tif", "wb") as raster:
+    raster.write(raster_pop)
+
 admin = [
     "https://fdw.fews.net/api/feature.geojson?layer=4694",  # adm0
     "https://fdw.fews.net/api/feature.geojson?layer=4695",
